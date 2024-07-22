a293b853b3416470ed334017d380cbd70b2894cb
Yung71nKzH3 <vleonardo2006@gmail.com>
2024-07-22 15:20:15 +0100

    Addition of a contact and bug fixing as well as organisation

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 scrollToTopBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+const contactButton = document.getElementById('contactButton');
+const contactForm = document.getElementById('contactForm');
+
+contactButton.addEventListener('click', () => {
+  contactForm.style.display = 'block';
+});
+
+const closeButton = document.querySelector('.close');
+
+closeButton.addEventListener('click', () => {
+  contactForm.style.display = 'none';
+});
